2e10c6706a9501f4320b41f25cf8ec6be0f3facb
Napster <guillaume-perot@hotmail.fr>
2016-12-12 19:18:53 +0100

    Modification du gulpfile.js pour le dev front ajout de nouveaux modules etc..

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,107 @@
-const elixir = require('laravel-elixir');
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    rename      = require('gulp-rename'),
+    cssmin      = require('gulp-minify-css'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    jshint      = require('gulp-jshint'),
+    scsslint    = require('gulp-sass-lint'),
+    cache       = require('gulp-cached'),
+    prefix      = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
+    size        = require('gulp-size'),
+    imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant'),
+    plumber     = require('gulp-plumber'),
+    notify      = require('gulp-notify');
 
-require('laravel-elixir-vue-2');
+var path = {
+    'resources': {
+        'sass': './resources/assets/sass',
+        'js': './resources/assets/js',
+        'images': './resources/assets/images'
+    },
+    'public': {
+        'css': './public/assets/css',
+        'js': './public/assets/js',
+        'images': './public/assets/images'
+    },
+    'sass': './resources/assets/sass/**/*.scss',
+    'js': './resources/assets/js/**/*.js',
+    'views': './resources/views/**/*.php',
+    'images': './resources/assets/images/**/**'
+};
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
 
-elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+gulp.task('scss', function() {
+    var onError = function(err) {
+        notify.onError({
+            title:    "Gulp",
+            subtitle: "Failure!",
+            message:  "Error: <%= error.message %>",
+            sound:    "Beep"
+        })(err);
+        this.emit('end');
+    };
+
+    return gulp.src(path.resources.sass + '/app.scss')
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(sass())
+        .pipe(size({ gzip: true, showFiles: true }))
+        .pipe(prefix())
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest(path.public.css))
+        .pipe(reload({stream:true}))
+        .pipe(cssmin())
+        .pipe(size({ gzip: true, showFiles: true }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(path.public.css))
 });
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        files:["public/**"],
+        proxy: "http://e-lycee",
+        host: "http://e-lycee"
+    });
+});
+
+gulp.task('js', function() {
+    gulp.src(path.resources.js + '/app.js')
+        .pipe(uglify())
+        .pipe(size({ gzip: true, showFiles: true }))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(path.public.js))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('scss-lint', function() {
+    gulp.src(path.resources.sass + '/**/*.scss')
+        .pipe(cache('scsslint'))
+        .pipe(scsslint());
+});
+
+gulp.task('jshint', function() {
+    gulp.src(path.resources.js + '*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(path.resources.sass + '/**/*.scss', ['scss']);
+    gulp.watch(path.resources.js + '*.js', ['jshint', 'js']);
+    gulp.watch(path.resources.images + '*', ['imgmin']);
+});
+
+gulp.task('imgmin', function () {
+    return gulp.src(path.resources.images + '**/**')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(path.public.images));
+});
+
+gulp.task('default', ['browser-sync', 'js', 'imgmin', 'scss', 'watch']);
